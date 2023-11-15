8cd70a3fb10a6c3cae4ac20909d27a314f1e7316
juan-abia <juan.abia@ailylabs.com>
2023-11-15 22:31:53 +0100

    Try to debug this

--- src/agent.py
+++ src/agent.py
@@ -1,14 +1,15 @@
 import os
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain import LLMChain, PromptTemplate
-from langchain.memory import ConversationTokenBufferMemory, ChatMessageHistory
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationTokenBufferMemory
 
 
 def get_llm():
     load_dotenv()
 
-    model = ChatOpenAI(model_name="gpt-4-1106-preview")
+    model = ChatOpenAI(model_name="gpt-4")
     return model
 
 
@@ -32,7 +33,7 @@ def get_prompt():
     # with open(os.path.join(SRC_DIR, "data", "base.txt"), "r", encoding="utf8") as f:
     #     base = f.read()
 
-    template = ("System: Eres un asistente dispuesto a ayudar"
+    template = ("System: Eres un asistente dispuesto a ayudar. "
                 "Aqui empieza la interaccion con el usuario:\n"
                 "{history}\n"
                 "Human: {user_prompt}")
